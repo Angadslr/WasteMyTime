09c61d041cb5f3c2113ca7697b7595bee5de6fb3
Angadslr <156040915+Angadslr@users.noreply.github.com>
2025-11-09 11:56:41 -0800

    First upload unfinished
    Adding more websites soon.

---
+++ uselessWeb.js
@@ -0,0 +1,33 @@
+function giveMeUselessWeb() {
+    const uselessWebsites = [
+                    'https://tpusa.com/',
+                    'https://daxofficial.com/',
+                    'https://official-rodwave.com/',
+                    'https://www.amazon.com/dp/B004A2LW6A?tag=oddthings-20',
+                    'https://www.amazon.com/dp/B07GTJ3WK4?tag=oddthings-20',
+                    'https://breakingbad.fandom.com/wiki/Huell_Babineaux'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                ];
+
+    const randomIndex = Math.floor(Math.random() * uselessWebsites.length);
+    console.log("I am running a javascript for the second time!");
+    return uselessWebsites[randomIndex];
+
+}
+console.log("I am running a javascript for the first time!");
