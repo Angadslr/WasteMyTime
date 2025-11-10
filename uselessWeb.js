44d5e9f9ab2d0bb832c03faaa9369a310e259b04
Angadslr <156040915+Angadslr@users.noreply.github.com>
2025-11-10 00:51:43 -0800

    Add more useless websites to the list
    Reset websites

--- uselessWeb.js
+++ uselessWeb.js
@@ -1,12 +1,24 @@
 function giveMeUselessWeb() {
     const uselessWebsites = [
-                    'https://tpusa.com/',
-                    'https://daxofficial.com/',
-                    'https://official-rodwave.com/',
-                    'https://www.amazon.com/dp/B004A2LW6A?tag=oddthings-20',
-                    'https://www.amazon.com/dp/B07GTJ3WK4?tag=oddthings-20',
-                    'https://breakingbad.fandom.com/wiki/Huell_Babineaux'
-
+                    
+                    'https://www.windows93.net/',
+                    'https://www.fallingfalling.com/',
+                    'https://stars.chromeexperiments.com/',
+                    'https://paveldogreat.github.io/WebGL-Fluid-Simulation/',
+                    'http://weavesilk.com/',
+                    'https://sandspiel.club/',
+                    'https://www.koalastothemax.com/',
+                    'https://cursoreffects.com/',
+                    'https://windows96.net/',
+                    'https://pointerpointer.com/',
+                    'https://www.zoomquilt.org/',
+                    'https://david.li/paint/',
+                    'https://sandboxels.r74n.com/',
+                    'https://29a.ch/sandbox/2011/neonflames/',
+                    'https://www.escapemotions.com/experiments/flame/',
+                    'https://www.shadertoy.com/view/XtGGRt',
+                    'https://bouncingdvdlogo.com/',
+                    'https://www.myphysicslab.com/engine2D/newtons-cradle-en.html'
 
 
 
